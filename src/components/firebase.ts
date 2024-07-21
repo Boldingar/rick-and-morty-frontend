5501a05e388c5f294b1ef97a5b17b3bc8761da8a
Mohammad Ahmad <m.a.7snen@gmail.com>
2024-07-22 01:52:22 +0300

    Minor Adjustments
    Created a .env file for the firebase evironment.
    Added missing modules.

--- src/components/firebase.ts
+++ src/components/firebase.ts
@@ -7,13 +7,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZL4Zvelu-zpwRTjt1JeuNh96dOmmbucE",
-  authDomain: "react-project-7743f.firebaseapp.com",
-  projectId: "react-project-7743f",
-  storageBucket: "react-project-7743f.appspot.com",
-  messagingSenderId: "713469952668",
-  appId: "1:713469952668:web:311e4f1330afdcd039e359",
-  measurementId: "G-9DDCN3K4XP",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
